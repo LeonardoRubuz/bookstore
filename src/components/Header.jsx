30a84466c7032947651c1bb2d712bd921961427c
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-03 00:35:23 +0100

    basic components and pages added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return ( 
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>About</li>
+                <li>Checkout</li>
+            </ul>
+        </header>
+     );
+}
+
+export default Header;
