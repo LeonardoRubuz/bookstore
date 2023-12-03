5db5fdec957779b0c34e3d5894828f7fbd4a0a88
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-12-03 01:04:46 +0100

    footer elements added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
 function Footer() {
     return ( 
         <footer>
-            Something
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+            <ul>
+                <li>Info</li>
+                <li>Support</li>
+                <li>Marketing</li>
+                <li>Terms of Use</li>
+                <li>Privacy Policy</li>
+            </ul>
+            <div>
+                <span>Copyright Icon  </span>
+                <span>2023</span>
+                <span>Leonardo Rubuz</span>
+            </div>
         </footer>
      );
 }
